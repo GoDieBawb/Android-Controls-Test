059a5af40bb53c74c33d491f1e5931def77eea70
Bawb <meancraft@hotmail.com>
2015-03-17 01:25:41 -0700

    Animations
    Model is now animated

--- Android Control Test/src/mygame/player/Player.java
+++ Android Control Test/src/mygame/player/Player.java
@@ -4,9 +4,12 @@
  */
 package mygame.player;
 
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  *
@@ -17,6 +20,8 @@ public class Player extends Node {
     private SimpleApplication      app;
     private BetterCharacterControl phys;
     private Node                   model;
+    private AnimControl            animControl;
+    private AnimChannel            animChannel;
     
     public Player(SimpleApplication app) {
         this.app = app;
@@ -38,6 +43,9 @@ public class Player extends Node {
         model.scale(.075f);
         model.setLocalTranslation(0,1,0);
         attachChild(model);
+        animControl = findAnimControl(model);
+        animChannel = animControl.createChannel();
+        animChannel.setAnim("FkIdle");
     }
     
     public Node getModel() {
@@ -45,12 +53,44 @@ public class Player extends Node {
     }
     
     public void run() {
-    
+        
+        if(!animChannel.getAnimationName().equals("FkWalk")) {
+            animChannel.setAnim("FkWalk");
+        }
+        
     }
     
     public void idle() {
-    
+        
+        if(!animChannel.getAnimationName().equals("FkIdle")) {
+            animChannel.setAnim("FkIdle");
+        }
+        
     }
+    
+    public AnimControl findAnimControl(final Spatial parent){
+        
+        AnimControl animControl = parent.getControl(AnimControl.class);
+        if (animControl != null) {
+            return animControl;
+        }
+        
+        if (parent instanceof Node) {
+            
+            for (final Spatial s : ((Node) parent).getChildren()) {
+                
+                final AnimControl animControl2 = findAnimControl(s);
+                
+                if (animControl2 != null) {
+                    return animControl2;
+                }
+                
+            }
+            
+        }
+        
+        return null;
+    }    
     
     public int getSpeedMult() {
         return 1;
